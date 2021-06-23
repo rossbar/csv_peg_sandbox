27f209223159ed8ff33d0076b161500513ab2e98
Ross Barnowski <rossbar@berkeley.edu>
2021-06-23 15:58:27 +0300

    Add memory profiling.

--- csv.py
+++ csv.py
@@ -30,16 +30,28 @@ def csvfile():
 parser = ParserPython(csvfile, ws='\t ')
 
 
+@profile
+def parse_with_peg(fname):
+    with open(fname, 'r') as fh:
+        test_data = fh.read()
+    parse_tree = parser.parse(test_data)
+    return parse_tree
+
+
+@profile
+def parse_with_loadtxt(fname):
+    a = np.loadtxt(fname, delimiter=",")
+    return a
+
+
 if __name__ == "__main__":
     import numpy as np
 
     # Generate test data
     rng = np.random.default_rng()
-    a = rng.random((1000, 5))
+    a = rng.random((10000, 5))
     fname = f"testdata_{a.dtype}_{a.shape[0]}x{a.shape[1]}.csv"
     np.savetxt(fname, a, fmt="%.2f", delimiter=",")
 
-    # Parse test data
-    with open(fname, 'r') as fh:
-        test_data = fh.read()
-    parse_tree = parser.parse(test_data)
+    parse_tree = parse_with_peg(fname)
+    b = parse_with_loadtxt(fname)
